cdc18927114e2c8dec2db51334059d78a4d2bb93
Kate <billykatessss@gmail.com>
2026-05-18 15:58:53 +0300

    second task

---
+++ js/getGoods.js
@@ -0,0 +1,23 @@
+const getGoods = () => {
+  const links = document.querySelectorAll(".navigation-link");
+
+  const getData = () => {
+    fetch("/db/db.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        localStorage.setItem("data", JSON.stringify(data));
+      });
+  };
+
+  getData();
+
+  links.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      getData();
+    });
+  });
+};
+
+getGoods();
--- js/search.js
+++ js/search.js
@@ -1,13 +1,9 @@
 const search = function () {
   const input = document.querySelector(".search-block > input");
   const searchBtn = document.querySelector(".search-block > button");
-  let searchValue = "";
 
-  input.addEventListener("input", (event) => {
-    searchValue = event.target.value;
-  });
   searchBtn.addEventListener("click", () => {
-    console.log(searchValue);
+    console.log(input.value);
   });
 };
 
